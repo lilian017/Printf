09b2eee110a5e814a47612bf9ac35e30dc788963
lilian017 <lilian.gitau211@gmail.com>
2023-06-26 10:31:23 -0700

    Task 1

--- main.h
+++ main.h
@@ -20,8 +20,15 @@ typedef struct format format_t;
 
 
 int _putchar(char c);
+int buffer (char);
 int _printf(const char *format, ...);
 int printf_int(va_list types, char buffer[], int flags, int width, int precision, int size);
+int printf_int(va_list);
 long int convert_size_number(long int n, int size);
+
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
 
 #endif

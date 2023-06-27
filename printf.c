a07ac146a42fc08cc09525b6e51b2d35eb56e647
Dan-njuguna <njugush020@gmail.com>
2023-06-27 22:56:09 +0300

    Printf

--- printf.c
+++ printf.c
@@ -44,103 +44,30 @@ int _printf(const char *format, ...)
 }
 
 /**
- * _spec_handler - funct that handles specifier
+ * _spec_handler - function that handles specifier
  * @lad: argument lists
  * @spec: specifier
- * @width: width of string
- * @length: length of string
  * Return: argument length
  */
-int _spec_handler(va_list lad, char spec, int width, char length)
+int _spec_handler(va_list lad, char spec)
 {
 	int lgth = 0;
 
 	if (spec == 'c')
 		lgth += _putchar(va_arg(lad, int));
 	else if (spec == 's')
-	{
-		char *str = va_arg(lad, char *);
-
-		if (width == -1)
-			lgth += _putstr(str);
-		else
-			lgth += _putstr_width(str, width);
-	}
+		lgth += _putstr(va_arg(lad, char *));
 	else if (spec == 'd' || spec == 'i')
-	{
-		if (length == 'l')
-			lgth += _putint(va_arg(lad, long int));
-		else if (length == 'h')
-			lgth += _putint(va_arg(lad, int));
-		else
-			lgth += _putint(va_arg(lad, int));
-	}
+		lgth += _putint(va_arg(lad, int));
+	else if (spec == 'b')
+		lgth += _putbit(va_arg(lad, unsigned int));
+	else if (spec == '%')
+		lgth += _putchar('%');
 	else if (spec == 'u')
-	{
-		if (length == 'l')
-			lgth += _putuint(va_arg(lad, unsigned long int));
-		else if (length == 'h')
-			lgth += _putuint(va_arg(lad, unsigned int));
-		else
-			lgth += _putuint(va_arg(lad, unsigned int));
-	}
+		lgth += _putuint(va_arg(lad, unsigned int));
 	else if (spec == 'o')
-	{
-		if (length == 'l')
-			lgth += _putoct(va_arg(lad, unsigned long int));
-		else if (length == 'h')
-			lgth += _putoct(va_arg(lad, unsigned int));
-		else
-			lgth += _putoct(va_arg(lad, unsigned int));
-	}
-	else if (spec == 'x')
-	{
-		if (length == 'l')
-			lgth += _puthex(va_arg(lad, unsigned long int), 0);
-		else if (length == 'h')
-			lgth += _puthex(va_arg(lad, unsigned int), 0);
-		else
-			lgth += _puthex(va_arg(lad, unsigned int), 0);
-	}
-	else if (spec == 'X')
-	{
-		if (length == 'l')
-			lgth += _puthex(va_arg(lad, unsigned long int), 1);
-		else if (length == 'h')
-			lgth += _puthex(va_arg(lad, unsigned int), 1);
-		else
-			lgth += _puthex(va_arg(lad, unsigned int), 1);
-	}
+		lgth += _putoct(va_arg(lad, unsigned int));
 	else
 		lgth += _printf("%%%c", spec);
-
-	return (lgth);
-}
-/**
- * _putstr_width - print a string with a given field width
- * @str: input string
- * @width: field width
- * Return: number of characters printed
- */
-int _putstr_width(char *str, int width)
-{
-	int lgth = 0;
-
-	if (!str)
-		str = "(null)";
-
-	while (*str)
-	{
-		lgth += _putchar(*str);
-		str++;
-		width--;
-	}
-
-	while (width > 0)
-	{
-		lgth += _putchar(' ');
-		width--;
-	}
-
 	return (lgth);
 }

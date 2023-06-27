89c05d0082aae0c1d3c829db0233ce8dd3d9fe94
Dan-njuguna <njugush020@gmail.com>
2023-06-27 10:20:35 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -12,39 +12,58 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int buff_ind = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
 	char buffer[BUFF_SIZE];
-	va_list lad;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(lad, format);
+	va_start(list, format);
 
-	while (*format != '\0')
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		buffer[buff_ind++] = *format;
-		if (buff_ind == BUFF_SIZE - 1)
+		if (format[i] != '%')
 		{
-			write(1, buffer, buff_ind);
-			buff_ind = 0;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
-		buffer[buff_ind++] = *format;
-		format++;
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
-	buffer[buff_ind] = '\0';
-	write(1, buffer, buff_ind);
-	va_end(lad);
-	return (0);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
+
 /**
- * print_buffer -prints contents of buffer if it exists
+ * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind: Index to add next char, rep length
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
+
 	*buff_ind = 0;
 }

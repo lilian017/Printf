9e3b2e77a3d519efb37034cbd5687a9ce7cc4009
Dan-njuguna <njugush020@gmail.com>
2023-06-27 11:36:51 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -8,49 +8,45 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list lad;
-	char buffer[BUFF_SIZE];
+	int count = 0;
+	char c;
+	va_list args;
+	va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(lad, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
+	while ((c = *format++) != '\0')
 	{
-		if (format[i] != '%')
+		if (c == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			c = *format++;
+			switch (c)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					count += fputs(va_arg(args, const char*), stdout);
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(c);
+					count += 2;
+					break;
+			}
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			putchar(c);
+			count++;
 		}
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(lad);
-
-	return (printed_chars);
+	va_end(args);
+	return count;
 }
-
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
---
+++ main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("Hello, World!\n");
+	return (0);
+}

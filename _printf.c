359bb5b7d1dcd9ae806e8dfae874331ba71c33b9
Dan-njuguna <njugush020@gmail.com>
2023-06-27 10:12:10 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+
+#define BUFF_SIZE 1024
+
 void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - produces output according to a format
@@ -9,55 +12,28 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, printed = 0, printed_ch = 0;
-	int f, w, p, s, buff_ind = 0;
-	va_list lad;
+	int buff_ind = 0;
 	char buffer[BUFF_SIZE];
+	va_list lad;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(lad, format);
 
-	while ((format && format[i]) != '\0')
+	while (*format != '\0')
 	{
-		if (format[i] != '%')
+		buffer[buff_ind++] = *format;
+		if (buff_ind == BUFF_SIZE - 1)
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-			{
-				print_buffer(buffer, &buff_ind);
-				/* write(1, &format[i], 1); */
-				printed_ch++;
-			}
-			else
-			{
-				print_buffer(buffer, &buff_ind);
-				f = get_flags(format, &i);
-				w = get_width(format, &i, list);
-				p = get_precision(format, &i, list);
-				s = get_size(format, &i);
-				++i;
-				printed = handle_print(format, &i, list, buffer, f, w, p, s);
-				if (printed == -1)
-					return (-1);
-				printed_ch += printed;
-			}
+			write(1, buffer, buff_ind);
+			buff_ind = 0;
 		}
-		i++;
+		buffer[buff_ind++] = *format;
+		format++;
 	}
-	print_buffer(buffer, &buff_ind);
+	buffer[buff_ind] = '\0';
+	write(1, buffer, buff_ind);
 	va_end(lad);
-	return (printed_ch);
-}
-/**
- * print_buffer -prints contents of buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index to add next char, rep length
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
+	return (0);
 }
